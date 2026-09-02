fb3bab8974741ead34cc3729f022da1f54057870
srikari1617 <155005897+srikari1617@users.noreply.github.com>
2026-09-02 16:52:35 +0530

    Time: 6 ms (20.93%) | Memory: 45.6 MB (94.04%) - LeetSync

---
+++ 9-palindrome-number/palindrome-number.java
@@ -0,0 +1,14 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+       // if(x == 0) return false;
+        if(x<0) return false;
+        int num = x;
+        int rev =0;
+        while(num>0){
+            int lastdigit = num%10;
+            rev=(rev*10)+lastdigit;
+            num=num/10;
+        }
+        return (rev == x);
+    }
+}
